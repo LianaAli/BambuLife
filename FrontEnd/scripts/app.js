e3c2360bbcb9cef09fe6579a722ffa255ae93874
Liana Ali <nurliana.kamilah@gmail.com>
2018-06-06 14:50:50 +0800

    Front-end Test: Display List of People in SW (Details)
    Basic display

--- FrontEnd/scripts/app.js
+++ FrontEnd/scripts/app.js
@@ -1,8 +1,8 @@
 angular
 .module('bambu', [])
 .factory('starwars', function($http){
-	var people = function(pageNum){
-		return $http.get(pageNum);
+	var people = function(pageUrl){
+		return $http.get(pageUrl);
 	}
 
 	return {
@@ -10,8 +10,8 @@ angular
 	}
 })
 .controller('mainController', function($scope, starwars){
-	$scope.getPeople = function(pageNum){
-		starwars.people(pageNum)
+	$scope.getPeople = function(pageUrl){
+		starwars.people(pageUrl)
 		.then(function(response){
 			$scope.pageContent = response.data;
 		})
