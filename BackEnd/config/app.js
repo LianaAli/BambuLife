a78c8b510e9e2ffd2f3bd73526a5aef59289e3af
Liana Ali <nurliana.kamilah@gmail.com>
2018-06-08 03:35:36 +0800

    Back-end Test: List of API Endpoints in Console

--- BackEnd/config/app.js
+++ BackEnd/config/app.js
@@ -9,4 +9,8 @@ app.use(bodyParser.json());
 const routes = require('../api/routes/UserRoute');
 routes(app);
 
+const endpointList = app._router.stack.filter(r => r.route).map(r => Object.keys(r.route.methods).join(',') + ' => ' + r.route.path);
+console.log('LIST OF API ENDPOINTS');
+console.log(endpointList);
+
 module.exports = app;
